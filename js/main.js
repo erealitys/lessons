a5171b5d8edf10e6a756f225c4ce408b3efe1cde
erealitys <erealitys@gmail.com>
2021-11-30 20:18:21 +0300

    JS для модального окна

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const btnCart = document.getElementById('cart')
+const modal = document.querySelector('.modal')
+const btnClose = document.querySelector('.btn-close')
+
+const modalOpen = () => {
+   modal.classList.toggle('modal-close')
+  
+}
+
+btnCart.addEventListener('click', modalOpen);
+btnClose.addEventListener('click', modalOpen); 
+
+modal.addEventListener ('click', (event) => {
+    if (event.target == modal) {
+        modalOpen()
+    }
+})
